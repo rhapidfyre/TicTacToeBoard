244170584a442bfa93f43e59b2f9e0ac07cfcad6
RhapidFyre <maharris84@gmail.com>
2019-11-25 00:24:41 -0800

    Playing with it to see why it's not working

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -135,7 +135,7 @@ TEST(TicTacToeBoard, playAfterWin) {
   board.placePiece(0,1);
   board.placePiece(0,2);
   board.getWinner();
-  ASSERT_EQ(board.placePiece(2,2), O);
+  ASSERT_EQ(board.placePiece(2,2), Invalid);
 }
 
 TEST(TicTacToeBoard, notFinished) {
